fef8896ed91028571fae0d91ff7e93f818780c22
trinhvithanh147 <trinhvidanhthanh@gmail.com>
2024-07-28 20:38:41 +0700

    Gan hoan thanh

--- js/back-to-top/main.js
+++ js/back-to-top/main.js
@@ -3,7 +3,7 @@
 	var backTop = document.getElementsByClassName('js-cd-top')[0],
 		offset = 300, // browser window scroll (in pixels) after which the "back to top" link is shown
 		offsetOpacity = 1200, //browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-		scrollDuration = 700,
+		scrollDuration = 150,
 		scrolling = false;
 
 	if( backTop ) {
